9ea1cbfb906af1b0417b63e1296243fa79401ead
hevizy <hevizy134@gmail.com>
2026-08-10 18:00:03 +0300

    update hashing password functions

--- app/core/security.py
+++ app/core/security.py
@@ -1,8 +1,10 @@
 import bcrypt
 
 def get_password_hash(password: str):
+    password = password.encode('utf-8')
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-    return hashed
+    hashed_string = hashed.decode('utf-8')
+    return hashed_string
 
 def check_password(password: str, hashed_password: str):
     return bcrypt.checkpw(password.encode(), hashed_password.encode())
