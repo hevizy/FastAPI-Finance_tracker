4c5f3f7df1ee0e38a6fc13e9446f6d461c7063c3
hevizy <hevizy134@gmail.com>
2026-08-11 11:14:37 +0300

    transaction service

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -5,21 +5,13 @@ from database.db import SessionDep
 from schemas.transaction import TransactionCreate
 from models.transaction import Transaction
 
+from services import transaction as transaction_service
+
 transactions_router = APIRouter(prefix="/transactions",tags=["transactions"],)
 
 @transactions_router.post("/", response_model=Transaction)
 def create_transaction(transaction_in: TransactionCreate, session: SessionDep) -> Transaction:
-    db_transaction = Transaction(
-        amount = transaction_in.amount,
-        type = transaction_in.type,
-        description = transaction_in.description,
-        category_id=0,
-        user_id=0
-    )
-    session.add(db_transaction)
-    session.commit()
-    session.refresh(db_transaction)
-    return db_transaction
+    return transaction_service.create_transaction(session, transaction_in)
 
 @transactions_router.get("/", response_model=list[Transaction])
 def read_transactions(
@@ -27,15 +19,11 @@ def read_transactions(
         offset: int = 0,
         limit: int = 100,
 ) -> list[Transaction]:
-    transactions = session.query(Transaction).offset(offset).limit(limit).all()
-    return transactions
+    return transaction_service.get_all_transactions(session, offset, limit)
 
-@transactions_router.get("/{id}", response_model=Transaction)
+@transactions_router.get("/{transaction_id}", response_model=Transaction)
 def read_transaction(
         session: SessionDep,
         transaction_id: int,
 ) -> Transaction:
-    transaction = session.query(Transaction).filter(Transaction.id == transaction_id).one_or_none()
-    if not transaction:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Transaction not found")
-    return transaction
+    return transaction_service.get_transaction_by_id(session, transaction_id)
---
+++ app/services/transaction.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException
+from sqlmodel import Session, select
+from models.transaction import Transaction
+from schemas.transaction import TransactionCreate
+
+def create_transaction(session: Session, transaction_in: TransactionCreate) -> Transaction:
+    transaction = Transaction(
+        amount=transaction_in.amount,
+        type=transaction_in.type,
+        description=transaction_in.description,
+        user_id=1, #!!! Использовать айди пользователя который создал транзакцию
+        category_id=1 #!!!Использовать айди категории, которую выбрал пользователь
+    )
+    session.add(transaction)
+    session.commit()
+    session.refresh(transaction)
+
+    return transaction
+
+def get_all_transactions(session: Session, offset: int, limit: int) -> list[Transaction]:
+    transactions = session.exec(select(Transaction).offset(offset).limit(limit)).all()
+    return transactions
+
+def get_transaction_by_id(session: Session, transaction_id: int) -> Transaction:
+    transaction = session.get(Transaction, transaction_id)
+    if not transaction:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+    return transaction
