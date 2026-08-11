e094fdec29446d16cf8f594b694bd4bd753cfbb8
hevizy <hevizy134@gmail.com>
2026-08-11 19:21:59 +0300

    small changes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from database import db
 from routers.users import users_router
 from routers.categories import categories_router
 from routers.transactions import transactions_router
